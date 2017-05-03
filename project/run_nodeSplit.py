e442e46841cd12c96972d1259e7fad5b522ba3b8
lyobo <305335475@qq.com>
2017-05-03 12:47:28 +0800

    增加单独的结点分割接口

---
+++ project/run_nodeSplit.py
@@ -0,0 +1,13 @@
+from qgis.core import *
+from PyQt4.QtGui import *
+import sys
+import dialog
+
+
+app = QApplication([])
+qgisPath = 'C:\\Program Files (x86)\\QGIS 2.16.0\\apps\\qgis'
+QgsApplication.setPrefixPath(qgisPath, True)
+QgsApplication.initQgis()
+main_win = dialog.MainWindow()
+main_win.show()
+sys.exit(app.exec_())
